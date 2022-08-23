50b5923705a64dfb8defd051503f9dbb27e92840
sheilaotwe <so3cg@virginia.edu>
2022-08-22 18:41:58 -0700

    adding lists header file and check cycle

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+/**
+ * check_cycle - function that checks if loop exists in cycle
+ * @list: struct to check
+ * Return: 1 if it is, 0 if not
+ */
+
+int check_cycle(listint_t *list)
+{
+	listint_t *turtle = list;
+	listint_t *hare = list;
+
+	if (list == NULL)
+		return (0);
+	while (turtle != NULL && hare != NULL && hare->next != NULL)
+	{
+		turtle = turtle->next;
+		hare = (hare->next)->next;
+		if (turtle == hare)
+			return (1);
+	}
+	return (0);
+}
