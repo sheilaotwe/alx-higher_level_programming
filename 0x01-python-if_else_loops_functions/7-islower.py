582765361f87f3034ea195ff7f3f81e373548da3
sheilaotwe <so3cg@virginia.edu>
2022-08-23 17:04:04 -0700

    check lowercase

---
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+This checks for lowercase character
+"""
+
+def islower(c):
+    for i in range(ord('a'), ord('{')):
+        if i == ord(c):
+            return True
+        return False
